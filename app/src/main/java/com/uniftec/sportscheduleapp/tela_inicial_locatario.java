c51ef420feedc6e4e51638aabb16e24a94404fb2
Gustavo <gustavo.canello@outlook.com>
2021-10-08 15:17:05 -0300

    tela de abertura - em andamento

---
+++ app/src/main/java/com/uniftec/sportscheduleapp/tela_inicial_locatario.java
@@ -0,0 +1,14 @@
+package com.uniftec.sportscheduleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_inicial_locatario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial_locatario);
+    }
+}
