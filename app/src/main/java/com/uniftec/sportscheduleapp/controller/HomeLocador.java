260354859d4c0e7895eed9997432608d567a247a
Leonardo-NB <leonardo.ferreira94@hotmail.com>
2021-10-13 09:12:58 -0300

    Criado layout activity_tela_inicial_locador.xml

---
+++ app/src/main/java/com/uniftec/sportscheduleapp/controller/HomeLocador.java
@@ -0,0 +1,16 @@
+package com.uniftec.sportscheduleapp.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uniftec.sportscheduleapp.R;
+
+public class HomeLocador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial_locador);
+    }
+}
