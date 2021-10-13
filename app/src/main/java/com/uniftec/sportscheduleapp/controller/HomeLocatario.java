c902b01d6f98d703f4a4482f25ecc7f4a7e76597
Gustavo <gustavo.canello@outlook.com>
2021-10-12 22:30:03 -0300

    Alteração Gustavo

--- app/src/main/java/com/uniftec/sportscheduleapp/controller/HomeLocatario.java
+++ app/src/main/java/com/uniftec/sportscheduleapp/controller/HomeLocatario.java
@@ -1,16 +1,37 @@
 package com.uniftec.sportscheduleapp.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.uniftec.sportscheduleapp.R;
 
 public class HomeLocatario extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial_locatario);
+
+
+        ConstraintLayout botaoAjuda = findViewById(R.id.btajuda);
+
+        botaoAjuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irFtec = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ftec.com.br"));
+                startActivity(irFtec);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/uniftec/sportscheduleapp/controller/LayoutListaQuadras.java
@@ -0,0 +1,16 @@
+package com.uniftec.sportscheduleapp.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uniftec.sportscheduleapp.R;
+
+public class LayoutListaQuadras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_lista_quadras);
+    }
+}
