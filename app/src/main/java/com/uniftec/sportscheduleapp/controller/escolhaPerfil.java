373b185ad18bccd9f9e3fd54e03d941b70bcbcbf
Leonardo-NB <leonardo.ferreira94@hotmail.com>
2021-10-14 13:27:39 -0300

    Ajuste de ícones.

---
+++ app/src/main/java/com/uniftec/sportscheduleapp/controller/escolhaPerfil.java
@@ -0,0 +1,20 @@
+package com.uniftec.sportscheduleapp.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uniftec.sportscheduleapp.R;
+
+public class escolhaPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolha_perfil);
+    }
+
+
+}
