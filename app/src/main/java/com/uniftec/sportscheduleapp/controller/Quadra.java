b21a8fe70cacc35ca40ff90c40e0e04636c4feff
Leonardo-NB <leonardo.ferreira94@hotmail.com>
2021-10-13 23:04:57 -0300

    Criado layout de opções da quadra - locador

---
+++ app/src/main/java/com/uniftec/sportscheduleapp/controller/Quadra.java
@@ -0,0 +1,16 @@
+package com.uniftec.sportscheduleapp.controller;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uniftec.sportscheduleapp.R;
+
+public class Quadra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadras);
+    }
+}
